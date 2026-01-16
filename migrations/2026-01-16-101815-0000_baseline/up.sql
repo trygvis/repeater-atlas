637ee6484dd97d245b1de8d44a84c7f18639d984
Trygve Laugstøl <trygvis@inamo.no>
2026-01-16 13:21:26 +0100

    wip

--- migrations/2026-01-16-101815-0000_baseline/up.sql
+++ migrations/2026-01-16-101815-0000_baseline/up.sql
@@ -9,11 +9,11 @@ CREATE TABLE ham_club
 
 CREATE TABLE repeater
 (
-    id        BIGSERIAL PRIMARY KEY,
-    ham_club  BIGINT REFERENCES ham_club,
-    call_sign VARCHAR NOT NULL UNIQUE,
-    frequency  BIGINT NOT NULL,
-    rx_offset  BIGINT NOT NULL,
+    id         BIGSERIAL PRIMARY KEY,
+    ham_club   BIGINT REFERENCES ham_club,
+    call_sign  VARCHAR NOT NULL UNIQUE,
+    frequency  BIGINT  NOT NULL,
+    rx_offset  BIGINT  NOT NULL,
     tx_subtone NUMERIC,
     rx_subtone NUMERIC
 );
@@ -28,6 +28,6 @@ CREATE TABLE repeater_change_log
 
 CREATE TABLE ham_operator
 (
-    id   BIGSERIAL PRIMARY KEY,
+    id        BIGSERIAL PRIMARY KEY,
     call_sign VARCHAR NOT NULL UNIQUE
 );
